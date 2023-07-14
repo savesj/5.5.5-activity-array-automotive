625575b544d8a9b37c13190df1d8c01d2c4b6edd
savesj <jesuslares.20@hotmail.com>
2023-07-13 21:28:50 -0400

    ACITIVITY5

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,35 +1,51 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
-
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
-
-
-//After you write the derived Car class, you should test it out.
-
-//Note: You can code your derived Car class here or make a file named index.js and do it there.
-
-
-//TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
-
-
-
-
-
-
-
-
-
-
-
-
-//TO DO: Creating Instances and Testing Them
-
-//You can use the same instance "v" of the Vehicle class above for the base class.
-
-
-
-
-
-//Create at least two new instances of the Car class and test them here:
+const VehicleModule = require("./vehicle").Vehicle;
+// add class that extends the vehicle class
+class Car extends VehicleModule {
+  constructor(make, model, year, color, mileage) {
+    super(make, model, year, color, mileage);
+    this.maxPassengers = 5;
+    this.passenger = 0;
+    this.numberOfWheels = 4;
+    this.maxSpeed = 160;
+    this.fuel = 10;
+    this.scheduleService = false;
+  }
+  // if passenger less /equal than maximumPassengers then availableRoom == true
+  loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+  // if fuel is greater than 0, then start == true
+  start() {
+    if (this.fuel > 0) {
+      console.log("Engine is on");
+      return (this.started = true);
+    } else {
+      console.log("Car needs fuel");
+      return (this.started = false);
+    }
+  }
+  // if mileage is greater than 30000, check for maintenance == true
+  checkService() {
+    if (this.mileage > 30000) {
+      this.scheduleService = true;
+      return this.scheduleService;
+    }
+  }
+}
+// console.log({ Car });
+let myCar = new Car("Lamborghini", "URUS", "2022", "black", 5000);
+console.log(myCar);
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+console.log(myCar)
--- vehicle.js
+++ vehicle.js
@@ -9,15 +9,15 @@ class Vehicle {
         this.mileage = mileage;
         this.started = false;
         this.numberOfWheels = 0;
+        this.fuel = 100
     }
-
     start() {
         if (this.fuel > 0) {
             return this.started = true;
-            console.log("engine started...!!!");
+            console.log('engine started');
         } else {
             return this.started = false;
-            console.log("engine cannot start...");
+            console.log('engine does not start');
         }
     }
     accelerate() {
@@ -52,40 +52,20 @@ class Vehicle {
         }
     }
     stop() {
+        console.log("Engine Off")
         this.started = false;
     }
-
-    drive() {
-        accelerate();
-    }
-    brake() {
-        decelerate();
-    }
-
-    autoPark()
-    {
-
-    }
-
-    autoDrive()
-    {
-      
-    }
-
-    typeOfVehicle(wheels) {
-        if (this.numberOfWheels == 8 && 8 == wheels) {
+    typeOfVehicle() {
+        if (this.numberOfWheels == 8) {
             console.log(this.model + " " + this.make + " is a Truck");
-        } else if (this.numberOfWheels == 4 && 4 == wheels) {
-            console.log(this.model + " " + this.make + " is a CAr");
-        } else if (this.numberOfWheels == 2 && 2 == wheels) {
+        } else if (this.numberOfWheels == 4) {
+            console.log(this.model + " " + this.make + " is a Car");
+        } else if (this.numberOfWheels == 2) {
             console.log(this.model + " " + this.make + " is a Bike");
         } else {
             console.log("Unknown type of vehicle");
         }
     }
 }
-
 //This exports things you want to use from this "module", more info in readme
-module.exports = {
-    Vehicle
-}
+export default { Vehicle };
